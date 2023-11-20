34bdfe295b8007abd3bdd2782fbf0b6342d0300f
varquhaz <jjvq02@gmail.com>
2023-11-20 16:38:37 -0400

    Initial

--- app/src/main/java/com/varqulabs/mytimecapsule/MainActivity.kt
+++ app/src/main/java/com/varqulabs/mytimecapsule/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Time Capsule")
+                    Greeting("Time Capsule App")
                 }
             }
         }
